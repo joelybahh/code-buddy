73a62d5a32a5d53e82200bc49fc2f1791f2576cc
Joel Gabriel <joel@inspacexr.com>
2023-05-08 12:07:32 +1000

    docs: update commit type description for docs [no-key]
    Clarify that 'docs' commit type includes changes to README, CHANGELOG, CONTRIBUTING, and code comments.

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -126,7 +126,7 @@ The rules for the commit message are as follows:
 }followed by a colon (:). The scope is optional.
 - feat: A new feature
 - fix: A bug fix
-- docs: Documentation only changes
+- docs: Documentation only changes, such as README, CHANGELOG, CONTRIBUTING, or code comments
 - style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 - refactor: A code change that neither fixes a bug nor adds a feature
 - test: Adding missing tests or correcting existing tests
