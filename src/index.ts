05a924ab222ee22c2f566bf9072982dfddd0daf9
Joel Gabriel <joel@inspacexr.com>
2023-05-15 09:25:12 +1000

    feat: 🎉 add new options to commit-all command
    Added breaking, message, scope, type, and issue options to the commit-all command for better customization and flexibility.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,18 @@ import { Command } from "commander";
 import { commitAllPrompts } from "./prompts/index.js";
 
 const program = new Command();
+
 program
     .version("0.2.0")
     .command("commit-all")
     .description(
         "Generates a commit message for each scope via GPT and commits all staged changes with the generated message."
     )
+    .option("-b, --breaking", "Mark the commit as a breaking change.")
+    .option("-m, --message <message>", "Specify the commit message.")
+    .option("-s, --scope <scope>", "Specify the scope.")
+    .option("-t, --type <type>", "Specify the type.")
+    .option("-i, --issue <issue>", "Specify the issue key.")
     .action(commitAllPrompts);
 
 program.parse(process.argv);
