cb34ccf59093659e88e631412d9ac6c327ee4bec
Joel Gabriel <joel@inspacexr.com>
2023-05-27 15:36:03 +1000

    revert: removed  as its not a useful arg

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ program
         "Generates a commit message for each scope via GPT and commits all staged changes with the generated message."
     )
     .option("-b, --breaking", "Mark the commit as a breaking change.")
-    .option("-m, --message <message>", "Specify the commit message.")
     .option("-s, --scope <scope>", "Specify the scope.")
     .option("-t, --type <type>", "Specify the type.")
     .option("-i, --issue <issue>", "Specify the issue key.")
