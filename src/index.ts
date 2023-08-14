7b95180ff2850f58212a9852e71a260d672c5eba
Joel Gabriel <joel@inspacexr.com>
2023-08-14 10:54:27 +1000

    refactor: 🧹 Refactor command setup in index.ts  [no-key]
    - Removed commitAllPrompts import and usage from index.ts
    - Imported setupCommitAll and setupSmartVersion from utils/commander.js
    - Used setupCommitAll and setupSmartVersion to set up commands for the program

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,13 @@
 
 import { Command } from "commander";
 
-import { commitAllPrompts } from "./prompts/index.js";
+import { setupCommitAll, setupSmartVersion } from "./utils/commander.js";
 
 const program = new Command();
 
-program
-    .version("0.2.0")
-    .command("commit-all")
-    .description(
-        "Generates a commit message for each scope via GPT and commits all staged changes with the generated message."
-    )
-    .option("-b, --breaking", "Mark the commit as a breaking change.")
-    .option("-s, --scope <scope>", "Specify the scope.")
-    .option("-t, --type <type>", "Specify the type.")
-    .option("-i, --issue <issue>", "Specify the issue key.")
-    .option("-r, --reason <reason>", "Specify the reason for the change.")
-    .action(commitAllPrompts);
+program.version("0.2.0");
+
+setupCommitAll(program);
+setupSmartVersion(program);
 
 program.parse(process.argv);
