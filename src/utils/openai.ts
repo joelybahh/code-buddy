3502876f6eca89a2e98226196e7be70fca091d7c
Joel Gabriel <joel@inspacexr.com>
2023-10-04 17:25:04 +1100

    feat(utils): added new utility functions for handling changelog and package.json files  [no-key]
    - Added loadChangelog, loadPackageJson, writeChangelog, and writePackageJson functions in openai.ts
    - Imported fs module for file operations
    - Imported ScopeMode from types/index.js

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -5,8 +5,9 @@ import {
     OpenAIApi,
 } from "openai";
 
+import fs from "fs";
 import path from "path";
-import { CodeBuddyConfig, CommitMessageError, ConfigLoadError } from "../types/index.js";
+import { CodeBuddyConfig, CommitMessageError, ConfigLoadError, ScopeMode } from "../types/index.js";
 
 const DEFAULT_MAX_TOKENS = 200;
 const DEFAULT_TEMPERATURE = 0.3;
@@ -20,6 +21,44 @@ export async function loadConfig(): Promise<CodeBuddyConfig> {
         return config.default;
     } catch (error) {
         throw new ConfigLoadError("Error loading configuration file: " + error.message);
+    }
+}
+
+export async function loadChangelog(): Promise<string> {
+    try {
+        const changelogPath = path.resolve(process.cwd(), "CHANGELOG.md");
+        const changelog = await fs.promises.readFile(changelogPath, "utf8");
+        return changelog;
+    } catch (error) {
+        throw new ConfigLoadError("Error loading changelog file: " + error.message);
+    }
+}
+
+export async function loadPackageJson(): Promise<any> {
+    try {
+        const packageJsonPath = path.resolve(process.cwd(), "package.json");
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+        return packageJson;
+    } catch (error) {
+        throw new ConfigLoadError("Error loading package.json file: " + error.message);
+    }
+}
+
+export async function writeChangelog(changelog: string): Promise<void> {
+    try {
+        const changelogPath = path.resolve(process.cwd(), "CHANGELOG.md");
+        await fs.promises.writeFile(changelogPath, changelog, "utf8");
+    } catch (error) {
+        throw new ConfigLoadError("Error writing to changelog file: " + error.message);
+    }
+}
+
+export async function writePackageJson(packageJson: any): Promise<void> {
+    try {
+        const packageJsonPath = path.resolve(process.cwd(), "package.json");
+        await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), "utf8");
+    } catch (error) {
+        throw new ConfigLoadError("Error writing to package.json file: " + error.message);
     }
 }
 
@@ -214,43 +253,77 @@ ${typeSpecificGuidelines}
 ${commitTypes}`;
 };
 
+const systemPromptMonorepo =
+    'Generate a changelog from the provided commit messages, organizing them under the scope with suggested version increments (+x.y.z). Include explanations for the suggested increments and organize the commits into "Major", "Minor", and "Revisions" categories. Omit any \'chore\' commits. If a commit contains \'BREAKING\' (placed after the commit message and before the issue key), suggest a major version increment. Include emojis if present in the commit messages.\n\n' +
+    "Given the following sample commit messages:\n\n" +
+    [
+        "feat(ui): 🎉 added usePrevious hook to the exports in inspace-ui [IN-889]\n\n- A new hook 'usePrevious' has been added to the exports in the hooks index file of inspace-ui package.",
+        "refactor(search): 🧹 changed 'sector' to 'sectors' in filter constants [IN-889]\n\n- Modified the type of sectors from 'sector' to 'sectors' in the defaultFilterSchema and defaultFilterSchemaWorkspaces objects within the inspace-search package.",
+        "refactor(api): 🧹 updated API endpoints BREAKING [IN-900]\n\n- The API endpoints were updated to improve performance and security. This change is not backward compatible.",
+    ].join("\n\n") +
+    "The expected output would be:\n\n" +
+    `# Changelog
+
+## ui +0.1.0
+
+The version increment is suggested due to the addition of new features that do not break backward compatibility and enhance the functionality of the \`ui\` scope.
+
+### Minor
+- 🎉 Added \`usePrevious\` hook to the exports in inspace-ui ([IN-889](link-to-issue-IN-889))
+  - A new hook 'usePrevious' has been added to the exports in the hooks index file of inspace-ui package.
+
+## search +0.0.1
+
+The version increment is suggested because the refactor enhances code quality and readability without adding new features or fixing bugs.
+
+### Revisions
+- 🧹 Changed 'sector' to 'sectors' in filter constants ([IN-889](link-to-issue-IN-889))
+  - Modified the type of sectors from 'sector' to 'sectors' in the defaultFilterSchema and defaultFilterSchemaWorkspaces objects within the inspace-search package.
+
+## api +1.0.0
+
+The introduction of breaking changes to the API warrants a major version increment to signify the non-backward-compatible modifications.
+
+### Major
+- 🧹 Updated API endpoints (BREAKING CHANGE) ([IN-900](link-to-issue-IN-900))
+  - The API endpoints were updated to improve performance and security. This change is not backward compatible.`;
+
+const getSystemPromptSingleRepo = (appName: string) =>
+    `For the app called ${appName}, generate a changelog from the provided commit messages, organizing them under the scope (if available) with suggested version increments (+x.y.z). Include explanations for the suggested increments and organize the commits into "Major", "Minor", and "Revisions" categories. Omit any \'chore\' commits. If a commit contains \'BREAKING\' (placed after the commit message and before the issue key), suggest a major version increment. Include emojis if present in the commit messages. \n\n` +
+    "Given the following sample commit messages:\n\n" +
+    [
+        "feat: added changelog command and updated commit-all command in src/index.ts  [no-key]\n\n- Imported generateChangelog from ./prompts/index.js\n- Initialized commit-all command with version 0.2.0\n- Added options for reason and destination branch\n- Initialized changelog command with description and option for destination branch",
+        "feat(utils): added getCommitLogs and getChangelog functions in utils  [no-key]\n\n- Added a new function 'getCommitLogs' in git.ts under utils.\n- Added another function 'getChangelog' in openai.ts under utils.",
+        "feat(types): add changelog destination to CodeBuddyConfig type  [no-key]\n\n- Added a new `changelog` field to the `CodeBuddyConfig` type in `src/types/index.ts`.",
+        "feat(prompts): added generateChangelog function and updated imports in prompts/index.ts  [no-key]\n\n- Added a new function generateChangelog to prompts/index.ts\n- Updated the import statement to include getCommitLogs from utils/git.js",
+    ].join("\n\n") +
+    "The expected output would be:\n\n" +
+    `# Changelog
+
+## +0.1.0
+
+The version increment is suggested due to the addition of new features that do not break backward compatibility and enhance the functionality of the project.
+
+### Minor
+- 🎉 Added changelog command and updated commit-all command in src/index.ts ([no-key](#))
+- Imported generateChangelog from ./prompts/index.js
+- Initialized commit-all command with version 0.2.0
+- Added options for reason and destination branch
+- Initialized changelog command with description and option for destination branch
+
+- 🎉 (utils) Added getCommitLogs and getChangelog functions in utils ([no-key](#))
+- Added a new function 'getCommitLogs' in git.ts under utils.
+- Added another function 'getChangelog' in openai.ts under utils.
+
+- 🎉 (types) Add changelog destination to CodeBuddyConfig type ([no-key](#))
+- Added a new \`changelog\` field to the \`CodeBuddyConfig\` type in \`src/types/index.ts\`.
+
+- 🎉 (prompts) Added generateChangelog function and updated imports in prompts/index.ts ([no-key](#))
+- Added a new function generateChangelog to prompts/index.ts
+- Updated the import statement to include getCommitLogs from utils/git.js`;
+
 export const getChangelog = async (commits: string) => {
     const [openai, config] = await getOpenAI();
-
-    const systemPrompt =
-        'Generate a changelog from the provided commit messages, organizing them under the scope with suggested version increments (+x.y.z). Include explanations for the suggested increments and organize the commits into "Major", "Minor", and "Revisions" categories. Omit any \'chore\' commits. If a commit contains \'BREAKING\' (placed after the commit message and before the issue key), suggest a major version increment. Include emojis if present in the commit messages.\n\n' +
-        "Given the following sample commit messages:\n\n" +
-        [
-            "feat(ui): 🎉 added usePrevious hook to the exports in inspace-ui [IN-889]\n\n- A new hook 'usePrevious' has been added to the exports in the hooks index file of inspace-ui package.",
-            "refactor(search): 🧹 changed 'sector' to 'sectors' in filter constants [IN-889]\n\n- Modified the type of sectors from 'sector' to 'sectors' in the defaultFilterSchema and defaultFilterSchemaWorkspaces objects within the inspace-search package.",
-            "refactor(api): 🧹 updated API endpoints BREAKING [IN-900]\n\n- The API endpoints were updated to improve performance and security. This change is not backward compatible.",
-        ].join("\n\n") +
-        "The expected output would be:\n\n" +
-        `# Changelog
-
-        ## ui +0.1.0
-        
-        The version increment is suggested due to the addition of new features that do not break backward compatibility and enhance the functionality of the \`ui\` scope.
-        
-        ### Minor
-        - 🎉 Added \`usePrevious\` hook to the exports in inspace-ui ([IN-889](link-to-issue-IN-889))
-          - A new hook 'usePrevious' has been added to the exports in the hooks index file of inspace-ui package.
-        
-        ## search +0.0.1
-        
-        The version increment is suggested because the refactor enhances code quality and readability without adding new features or fixing bugs.
-        
-        ### Revisions
-        - 🧹 Changed 'sector' to 'sectors' in filter constants ([IN-889](link-to-issue-IN-889))
-          - Modified the type of sectors from 'sector' to 'sectors' in the defaultFilterSchema and defaultFilterSchemaWorkspaces objects within the inspace-search package.
-        
-        ## api +1.0.0
-        
-        The introduction of breaking changes to the API warrants a major version increment to signify the non-backward-compatible modifications.
-        
-        ### Major
-        - 🧹 Updated API endpoints (BREAKING CHANGE) ([IN-900](link-to-issue-IN-900))
-          - The API endpoints were updated to improve performance and security. This change is not backward compatible.`;
 
     const userMessage = commits;
 
@@ -258,7 +331,10 @@ export const getChangelog = async (commits: string) => {
     const response = await openai.createChatCompletion({
         messages: [
             {
-                content: systemPrompt,
+                content:
+                    config.commit?.scope.mode === ScopeMode.Monorepo
+                        ? systemPromptMonorepo
+                        : getSystemPromptSingleRepo(config.changelog?.appName || "CodeBuddy"),
                 role: ChatCompletionRequestMessageRoleEnum.System,
             },
             {
